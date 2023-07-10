8495513300a5d0a24678c26b75a4c284af03ab83
aykacx <halilaykac@doamedia.co>
2023-07-10 17:19:53 +0300

    home_page_ok-styles added

--- home_page.php
+++ home_page.php
@@ -1,11 +1,80 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home page</title>
+    <title>HOME PAGE</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 400px;
+            margin: 100px auto;
+            background-color: #fff;
+            border-radius: 5px;
+            padding: 20px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        .welcome-message {
+            text-align: center;
+            font-size: 20px;
+            margin-bottom: 20px;
+        }
+
+        .info {
+            margin-bottom: 10px;
+        }
+
+        .logout-btn {
+            background-color: #f44336;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+            width: 100%;
+        }
+
+        .logout-btn:hover {
+            background-color: #d32f2f;
+        }
+    </style>
 </head>
+
 <body>
-    
+    <div class="container">
+        <h1>HOME PAGE</h1>
+        <?php
+        session_start();
+        if (isset($_SESSION['name'])) {
+            echo "<div class='welcome-message'>Welcome " . $_SESSION['name'] . "</div>";
+            echo "<div class='info'>Password: " . $_SESSION['pass'] . "</div>";
+        } else {
+            header("location:index.php");
+        }
+        ?>
+
+        <form method="POST">
+            <input class="logout-btn" type="submit" name="logout" value="LOGOUT">
+        </form>
+
+        <?php
+        if (isset($_POST['logout'])) {
+            session_destroy();
+            header("location:index.php");
+        }
+        ?>
+    </div>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,51 +1,113 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>LOGIN PAGE</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 400px;
+            margin: 100px auto;
+            background-color: #fff;
+            border-radius: 5px;
+            padding: 20px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+
+        input[type="text"],
+        input[type="password"] {
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+            margin-bottom: 15px;
+        }
+
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+            width: 100%;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+
+        .error-message {
+            color: red;
+            margin-top: 10px;
+        }
+
+        .security-code {
+            font-weight: bold;
+            text-align: center;
+            font-size: 20px;
+            margin-bottom: 10px;
+        }
+    </style>
 </head>
 
 <body>
-    <form action="" method="POST">
-        <label for="username">Username: </label>
-        <input class="textbox" id="username" type="text" name="username"><br>
-        <label for="password">Password: </label>
-        <input class="textbox" id="password" type="password" name="password"><br>
-        <label>Sercurity code: </label>
+    <div class="container">
+        <h1>LOGIN PAGE</h1>
+        <form action="" method="POST">
+            <label>Username:</label>
+            <input class="textbox" type="text" name="username">
+
+            <label>Password:</label>
+            <input class="textbox" type="password" name="password">
+
+            <label>Security Code:</label>
+            <div class="security-code">
+                <del>
+                    <?php $produce = substr(md5(microtime()), rand(0, 26), 6);
+                    echo $produce; ?>
+                </del>
+            </div>
+
+            <label>Enter security code:</label>
+            <input class="textbox" type="text" name="s_code">
+
+            <input class="button" type="submit" name="login" value="LOGIN">
+        </form>
+
         <?php
         session_start();
-        $sCode = '';
-        if (!empty($_SESSION['security_code'])) {
-            $sCode = $_SESSION['security_code'];
-        } else {
-            $sCode = substr(md5(microtime()), rand(0, 26), 6);
-            $_SESSION['security_code'] = $sCode;
+        if (isset($_POST['login'])) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $scode = $_POST['s_code'];
+            if ($username == "halil" && $password == "1234" && $scode == is_string($produce)) {
+                $_SESSION['name'] = $username;
+                $_SESSION['pass'] = $password;
+                header("location:home_page.php");
+            } else {
+                echo "<div class='error-message'>The entered information is incorrect.</div>";
+            }
         }
         ?>
-        <label><del>
-                <?php echo $sCode; ?>
-            </del></label><br>
-        <label>Enter security code</label>
-        <input class="textbox" id="security_code" type="text" name="security_code"><br>
-        <input class="button" type="submit" name="submit" value="Login">
-    </form>
-    <?php
-    if (isset($_POST['submit'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $security_code = $_POST['security_code'];
-
-        if ($username == 'halil' && $password == '1234' && $security_code == $sCode) {
-            $_SESSION['username'] = $username;
-            $_SESSION['password'] = $password;
-            header('location: home_page.php');
-        } else {
-            echo 'Login denied';
-        }
-    }
-    ?>
+    </div>
 </body>
 
 </html>
